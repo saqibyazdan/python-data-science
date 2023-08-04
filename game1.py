5d54099650a8a1cc0667ed461fde430db571f6f4
saqibyazdan <saqibyazdan.1088@gmail.com>
2023-08-05 01:11:51 +0530

    game1

--- game1.py
+++ game1.py
@@ -1,18 +1,17 @@
 # run this in terminal -> pip install pgzero
 import pgzrun
 from random import randint
-
 WIDTH = 800
 HEIGHT = 600
 score = 0
-music.play('got')
+music.play("avenger_piano")
 
 player = Actor("ironman", (WIDTH//2, HEIGHT//2))
-coin = Actor('coin', (randint(50, WIDTH-50),
-                      randint(50,HEIGHT-50)))
-alien = Actor('thanos', (-100 , HEIGHT//2,))
+coin = Actor('coin', (randint(50, WIDTH-50), 
+                      randint(50, HEIGHT-50)))
+alien = Actor('alien', (-100, HEIGHT//2,))
 ps = 5 # player speed
-es = 2 # enemy speed
+es = 1 # enemy speed
 def player_movement():
     global score
     if keyboard.left:
@@ -24,15 +23,17 @@ def player_movement():
     elif keyboard.up:
         player.y -= ps
     elif keyboard.down:
-        player.y += ps 
+        player.y += ps
     else:
-     player.angle = 0
-# collision detection
-     if player.colliderect(coin):
-         coin.x = randint(50, WIDTH -50)
-         coin.y = randint(50, HEIGHT-50)
-         sounds.action.play()
-         score = score +1
+        player.angle = 0
+    # collision detection
+    if player.colliderect(coin):
+        player.image = 'ironman'
+        coin.x = randint(50, WIDTH-50)
+        coin.y = randint(50, HEIGHT-50)
+        score += 1
+        sounds.action2.play()
+        
 
 def enemy_movement():
     global score
@@ -44,28 +45,27 @@ def enemy_movement():
         alien.y += es
     elif alien.y > player.y:
         alien.y -= es
-    if player.colliderect(alien):
-        score = score -1
-
-    
+    if alien.colliderect(player):
+        player.image = 'dead_ironman'
+        sounds.action3.play()
+        score = score-1
+        
     
 
 def draw():
-    screen.fill("white")
+    screen.fill("black")
     screen.draw.text(
         f"Score:{score}",
-        color= "black",
-        topleft= (10,10),
-        fontsize= 30
+        color="red",
+        topleft=(10, 10),
+        fontsize=30
     )
-
     player.draw()
     coin.draw()
     alien.draw()
+   
 
 def update(dt):
-    print(dt)
     player_movement()
     enemy_movement()
-
 pgzrun.go()
